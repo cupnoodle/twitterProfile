92d1d449ee0f32fb3c03a5d9179d8b110432048f
soulchild <soulchildsince2013@gmail.com>
2016-06-09 17:31:59 +0800

    Added avatar imageset, header imageset and table view
    The changes are :
    1. Added Avatar imageset
    2. Added Header imageset
    3. UITableView with source and delegate to ViewController.swift

--- twitterProfile/ViewController.swift
+++ twitterProfile/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tweetTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tweetTable.dataSource = self
+        self.tweetTable.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: - Table View source and delegate
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let reuseIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) //as! UITableViewCell
+        
+        cell.textLabel!.text = "Item " + String(indexPath.row)
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
